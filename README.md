315c6615640b6a43772472ec645d2d8019169602
FloDevelops <65234487+FloDevelops@users.noreply.github.com>
2023-10-05 01:38:42 -0400

    now looping through files

---
+++ README.md
@@ -0,0 +1,25 @@
+# HAR files to csv
+
+This is a simple script to convert HAR files to CSV files to quickly import network requests in Google Sheets.
+
+## Requirements
+
+- Python 3.11 or higher (may work with older versions but not tested)
+- Google Chrome
+
+## Installation
+
+1. Clone the repository or download the files.
+2. Create a virtual environment with `python -m venv venv` and activate it with `venv\Scripts\activate.bat` on Windows or `source venv/bin/activate` on Linux.
+3. Install the requirements with `pip install -r requirements.txt`.
+
+## Usage
+
+1. Open a Chrome private window on the desired start page and open the developer tools (F12) on the network tab. Clear previous logs and enable "Preserve log".
+2. Hard reload the page (Ctrl + F5) and wait for the page to load and all requests to finish.
+3. Accept or deny consents if necessary and browse a few pages.
+4. When done, click on the "Export HAR" button at the top of the network tab and save the file in the input_files_har folder.
+5. Repeat steps 1 to 4 for each domain to analyze.
+6. Run the script with the command `python main.py` from the terminal.
+7. The CSV files will be saved in the output_files_csv folder.
+8. Import the CSV files Google Sheets and analyze the data.
--- main.py
+++ main.py
@@ -1,25 +1,56 @@
 import json, csv
+from os import listdir
 from haralyzer import HarParser, HarPage
 
-def main():
-    with open('lehoublon.com.har', 'r') as f:
-        har_parser = HarParser(json.loads(f.read()))
+folders = {
+    'input': 'input_files_har',
+    'output': 'output_files_csv'
+}
 
-    print(har_parser.pages)
+
+
+def main():
+    for file in listdir(folders['input']):
+        if file == '.DS_Store' or file == '.gitkeep':
+            continue
+        with open(folders['input'] + '/' + file, 'r') as f:
+            parse_har(f)
+
+
+
+def parse_har(file):
+    har_parser = HarParser(json.loads(file.read()))
 
     output = []
     for entry in har_parser.har_data['entries']:
-        output.append({
-            'root_domain': '.'.join(entry['request']['url'].split('/')[2].split('.')[-2:]),
-            'url': entry['request']['url']
-        })
+        record = {
+            'request_root_domain': '.'.join(entry['request']['url'].split('/')[2].split('.')[-2:]),
+            'request_status': entry['response']['status'],
+            'request_url': entry['request']['url'],
+            'page': find_page(har_parser, entry.get('pageref', None))
+        }
+        output.append(record)
+    write_to_csv(output, file.name)
 
-    output = sorted(output, key=lambda k: k['root_domain'])
-    with open('output.csv', 'w') as f:
-        writer = csv.writer(f)
-        writer.writerow(['root_domain', 'url'])
+
+
+def find_page(har_parser, page_name):
+    for page in har_parser.har_data['pages']:
+        if page['id'] == page_name:
+            return page.get('title', None)
+    return None
+
+
+
+def write_to_csv(output, file_name):
+    output = sorted(output, key=lambda k: (k['request_root_domain'], k['request_url'], '' if k['page'] is None else k['page']))
+    with open(folders['output'] + '/' + '.'.join(file_name.split('/')[-1].split('.')[:-1]) + '.csv', 'w') as csvfile:
+        fieldnames = ['request_root_domain', 'request_status', 'request_url', 'page']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
         for row in output:
-            writer.writerow([row['root_domain'], row['url']])
+            writer.writerow(row)
+
 
 
 if __name__ == '__main__':
